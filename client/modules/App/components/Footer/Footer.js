12f2eae265675ec209456261f86df3481fc0e836
Brian <cbchien46@gmail.com>
2017-12-14 11:22:44 +0800

    Minor changes to initial set up

--- client/modules/App/components/Footer/Footer.js
+++ client/modules/App/components/Footer/Footer.js
@@ -10,8 +10,9 @@ import bg from '../../header-bk.png';
 export function Footer() {
   return (
     <div style={{ background: `#FFF url(${bg}) center` }} className={styles.footer}>
-      <p>&copy; 2016 &middot; Hashnode &middot; LinearBytes Inc.</p>
-      <p><FormattedMessage id="twitterMessage" /> : <a href="https://twitter.com/@mern_io" target="_Blank">@mern_io</a></p>
+      <p>B2L Project for Chingu Voyage &copy; 2017</p>
+      <p>Boilerplate template &copy; 2016 &middot; Hashnode &middot; LinearBytes Inc.</p>
+      <p><FormattedMessage id="twitterMessage" /> : <a href="https://twitter.com/" target="_Blank">@contact</a></p>
     </div>
   );
 }
